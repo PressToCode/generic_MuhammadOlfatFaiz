1925d29c23c6d8dbaa6f0dda876302b6a7e0f090
PressToCode <137992170+PressToCode@users.noreply.github.com>
2024-04-23 12:06:10 +0700

    Update GenericClassMultiple.java

--- [ T ] Kelas Praktisi 1/mavenproject1/src/main/java/com/mycompany/mavenproject1/GenericClassMultiple.java
+++ [ T ] Kelas Praktisi 1/mavenproject1/src/main/java/com/mycompany/mavenproject1/GenericClassMultiple.java
@@ -10,33 +10,36 @@ package com.mycompany.mavenproject1;
  * @param <T>
  * @param <U>
  */
-public class GenericClass<T,U> {
-    private T dataPertama;
-    private U dataKedua;
-    
-    // Constructor
-    class GenericClass(T dataPertama) {
-        this.dataPertama = dataPertama;
-    }
-    
-    class GenericClass(T dataFirst, U dataSecond) {
-        this.dataPertama = dataFirst;
-        this.dataKedua = dataSecond;
-    }
-    
-    class T getDataPertama() {
-        return this.dataPertama;
-    }
-    
-    class U getDataKedua() {
-        return this.dataKedua;
-    }
-    
-    class T setDataPertama(T dataFirst) {
-        this.dataPertama = dataFirst;
-    }
-    
-    class U setDataKedua(U dataSecond) {
-        this.dataKedua = dataSecond;
+public class GenericClassMultiple {
+    public class GenericClass<T,U> {
+        private T dataPertama;
+        private U dataKedua;
+        
+        // Constructor
+        public GenericClass(T dataPertama) {
+            this.dataPertama = dataPertama;
+        }
+        
+        public GenericClass(T dataFirst, U dataSecond) {
+            this.dataPertama = dataFirst;
+            this.dataKedua = dataSecond;
+        }
+        
+        public T getDataPertama() {
+            return this.dataPertama;
+        }
+        
+        public U getDataKedua() {
+            return this.dataKedua;
+        }
+        
+        public void setDataPertama(T dataFirst) {
+            this.dataPertama = dataFirst;
+        }
+        
+        public void setDataKedua(U dataSecond) {
+            this.dataKedua = dataSecond;
+        }
     }
 }
+
